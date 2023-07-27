bb14850e5b45d57e380c0a82c9d0607a61c0d37c
Anison <barsikstd@gmail.com>
2023-07-26 22:14:22 -0500

    commit

---
+++ contacts.js
@@ -0,0 +1,38 @@
+const fs = require("fs").promises;
+
+const contactsPath = __dirname + "/db/contacts.json";
+const readContacts = fs.readFile(contactsPath).then((data) => {
+  return JSON.parse(data);
+});
+
+async function listContacts() {
+  const allContacts = await readContacts;
+  console.table(allContacts);
+}
+
+async function getContactById(contactId) {
+  const allContacts = await readContacts;
+  console.log(allContacts.find((contact) => contact.id === contactId));
+}
+
+async function removeContact(contactId) {
+  const allContacts = await readContacts;
+  const newData = allContacts.filter((contact) => {
+    return contact.id !== contactId;
+  });
+  fs.writeFile(contactsPath, JSON.stringify(newData));
+}
+
+async function addContact(name, email, phone) {
+  const { nanoid } = await import("nanoid");
+  const allContacts = await readContacts;
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  allContacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+}
+module.exports = { listContacts, getContactById, removeContact, addContact };
---
+++ index.js
@@ -0,0 +1,61 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
+
+const options = {
+  action: {
+    describe: "action",
+    type: "string",
+    demandOption: true,
+    choices: ["list", "get", "add", "remove"],
+  },
+  id: {
+    describe: "user id",
+    type: "string",
+  },
+  name: {
+    describe: "new user name",
+    type: "string",
+  },
+  email: {
+    describe: "new user email",
+    type: "string",
+  },
+  phone: {
+    describe: "new user phone",
+    type: "string",
+  },
+};
+
+const argv = require("yargs/yargs")(process.argv.slice(2)).options(
+  options
+).argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  console.log("action: ", action);
+
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
